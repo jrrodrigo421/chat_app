7727b516799b54fbe2e87c58e1b4a1b625726a50
jrrodrigo42 <rjr89000@gmail.com>
2024-06-15 14:29:25 -0300

    feat: routes and subroutes

---
+++ app/user/page.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function User() {
+  return <h1>
+    Pagina do usuário
+  </h1>
+}
---
+++ app/user/settings/page.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Settings() {
+  return <h1>
+    Pagina do usuárioaaaa
+  </h1>
+}
